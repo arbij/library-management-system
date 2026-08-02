297e4341427430646e64d80f697eb10a577c69d4
Arbi <arbij2002@gmail.com>
2026-08-02 19:31:05 +0200

    added set up batch

--- databases/test/prisma/apply changes.bat
+++ databases/test/prisma/apply changes.bat
@@ -1,7 +1,5 @@
 @echo off
 
-cls
-
 call npx prisma generate
 
 npx prisma db push
---
+++ set up.bat
@@ -0,0 +1,10 @@
+@echo off
+
+call npm install
+
+pushd "databases\prod\prisma"
+	call "apply changes.bat"
+popd
+
+cd "databases\test\prisma"
+"apply changes.bat"
